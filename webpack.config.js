e050f81ee0b654a6842d8a6776f7a792a8b58950
Eric Kim <261835705@qq.com>
2019-06-25 16:10:23 +0800

    update webpack-dev-middleware and webpack-hot-middleware

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   name: 'webpack-build',
   target: 'node',
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, './dist'),
